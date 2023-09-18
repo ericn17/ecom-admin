0ceab21221d048aa6462f13d13c64e5e7bbfd521
Eric Nguyen <15enguyen4@gmail.com>
2023-09-18 14:42:24 -0700

    modal providers

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { Modal } from "../ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
 
 export const StoreModal = () => {
-    <Modal
-        title="Create Store"
-        description="Add a new store to manage products and categories"
-        isOpen={false}
-        onClose={() =>{}}
-    >
-        Future Create Store FOrm
-    </Modal>
+    const storeModal = useStoreModal();
+
+    return (
+        <Modal
+            title="Create Store"
+            description="Add a new store to manage products and categories"
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            Future Create Store FOrm
+        </Modal>
+    )
 }
