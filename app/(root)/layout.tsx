8dcd75bda78d8e8224edd361c7f301ddee0b1822
Eric Nguyen <15enguyen4@gmail.com>
2023-10-21 17:41:34 -0700

    route for navbar created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,1 +1,32 @@
-export default 
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+export default async function SetupLayout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect('/sign-in');
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            userId
+        }
+    });
+
+    if (!store) {
+        redirect (`/${store.id}`);
+    }
+
+    return (
+        <>
+        {children}
+        </>
+    );
+};
