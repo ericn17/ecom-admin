bff2aa336a1c0e11766bb32a0b3410d1ca921357
Eric Nguyen <15enguyen4@gmail.com>
2023-10-27 18:10:46 -0700

    navbar layout added, design for navbar to change

--- app/(dashboard)/[storeid]/layout.tsx
+++ app/(dashboard)/[storeid]/layout.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
+import Navbar from '@/components/navbar';
 
 import prismadb from "@/lib/prismadb"
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,20 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
     return (
-        <div>
-            This is the navbar.
-       </div> 
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                <div>
+                    This will be store switcher.
+                </div>
+                <div>
+                    This will be the routes
+                </div>
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton afterSignOutUrl="/" />
+                </div>
+            </div>
+        </div> 
     )
 }
 
